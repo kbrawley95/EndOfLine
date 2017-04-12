1d5ef39135d3203c16afa026a7961abed07700fb
kbrawley95 <kieran.brawley95@gmail.com>
2017-04-12 14:57:26 +0100

    TouchManager

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	//Update is called once per frame
+	void Update ()
+	{
+		for (int i = 0; i < TouchManager.TouchCount(); ++i)
+		{
+			if (TouchManager.GetTouch(i).phase.Equals(TouchPhase.Began))
+			{
+				//Construct a ray from the current touch coordinates
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay(TouchManager.GetTouch(i).position);
+				if (Physics.Raycast(ray, out hit))
+				{
+					//Destroy the first game object hit by ray
+					Destroy(hit.collider.gameObject);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class TouchManager 
+{
+    public static Touch GetTouch(int id)
+    {
+        #if UNITY_EDITOR
+            Touch mouse = new Touch();
+            mouse.position  = Input.mousePosition;
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+                mouse.phase = TouchPhase.Began;
+            else if (Input.GetKeyUp(KeyCode.Mouse0))
+                mouse.phase = TouchPhase.Ended;
+            else
+                mouse.phase = TouchPhase.Moved;
+
+            //TODO Can add more mouse stuff here if you like
+            return mouse;
+        #else
+            return Input.GetTouch(i);
+        #endif
+    }
+
+    public static int TouchCount()
+    {
+        #if UNITY_EDITOR
+            if (Input.GetKey(KeyCode.Mouse0))
+                return 1;
+            else
+                return 0;
+        #else   
+            return Input.touchCount;
+        #endif
+    }
+}
